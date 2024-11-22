da37d50ba572f1e5fe4a4ec8139bbdd8ddbf31ef
Micke Öhlén <tech@igpholding.com>
2024-11-22 12:33:20 +0100

    Update: Smooth fadein load to mitigate SSR vs hydrated content

--- components/MainBanner.vue
+++ components/MainBanner.vue
@@ -1,59 +1,69 @@
 <template>
-  <div v-if="loading" class="loading-placeholder">
-    <LoadingSpinner />
-  </div>
-  <div v-else-if="currentPromo" class="head-banner h-100 bg-jazzy-darkblue">
-    <div class="w-full">
-      <a :href="regLink" class="mb-[-5px]" v-if="currentPromo.acf?.image_small">
-        <picture>
-          <source 
-            media="(min-width: 992px)" 
-            :srcset="currentPromo.acf.image_full"
-            type="image/webp"
-            loading="eager"
-          >
-          <img 
-            :src="currentPromo.acf.image_small"
-            class="w-full h-auto pt-16 min-w-screen"
-            :alt="currentPromo.yoast_head_json?.description || 'Promotion Banner'"
-            :width="1920"
-            :height="400"
-            loading="eager"
-            fetchpriority="high"
-          >
-        </picture>
-      </a>
-    </div>
+  <ClientOnly>
+    <div class="head-banner">
+      <div v-if="loading" class="loading-placeholder">
+        <LoadingSpinner />
+      </div>
+      <div v-else-if="currentPromo" class="head-banner h-100 bg-jazzy-darkblue">
+        <div class="w-full">
+          <a :href="regLink" class="mb-[-5px]" v-if="currentPromo.acf?.image_small">
+            <picture>
+              <source 
+                media="(min-width: 992px)" 
+                :srcset="currentPromo.acf.image_full"
+                type="image/webp"
+                loading="eager"
+              >
+              <img 
+                :src="currentPromo.acf.image_small"
+                class="w-full h-auto pt-16 min-w-screen"
+                :alt="currentPromo.yoast_head_json?.description || 'Promotion Banner'"
+                :width="1920"
+                :height="400"
+                loading="eager"
+                fetchpriority="high"
+              >
+            </picture>
+          </a>
+        </div>
 
-    <BannerContent 
-      :sig-terms="currentPromo.acf?.sig_terms"
-      :one-two-three-icon="currentPromo.acf?.one_two_three_icon"
-      :trust-icons="currentPromo.acf?.trust_icons"
-    />
-    
-    <div class="bg-jazzy-beige">
-      <div class="container mx-auto py-4 md:py-8">
-        <div class="flex justify-center items-center">
-          <img 
-            src="/images/payments_jazzy_mobile.webp" 
-            alt="Payment Methods" 
-            class="w-full md:hidden"
-            loading="lazy"
-            width="640"
-            height="80"
-          />
-          <img 
-            src="/images/payments_jazzy.svg" 
-            alt="Payment Methods" 
-            class="w-3/4 hidden md:block"
-            loading="lazy"
-            width="960"
-            height="120"
-          />
+        <BannerContent 
+          :sig-terms="currentPromo.acf?.sig_terms"
+          :one-two-three-icon="currentPromo.acf?.one_two_three_icon"
+          :trust-icons="currentPromo.acf?.trust_icons"
+        />
+        
+        <div class="bg-jazzy-beige">
+          <div class="container mx-auto py-4 md:py-8">
+            <div class="flex justify-center items-center">
+              <img 
+                src="/images/payments_jazzy_mobile.webp" 
+                alt="Payment Methods" 
+                class="w-full md:hidden"
+                loading="lazy"
+                width="640"
+                height="80"
+              />
+              <img 
+                src="/images/payments_jazzy.svg" 
+                alt="Payment Methods" 
+                class="w-3/4 hidden md:block"
+                loading="lazy"
+                width="960"
+                height="120"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+
+    <template #fallback>
+      <div class="loading-placeholder">
+        <LoadingSpinner />
+      </div>
+    </template>
+  </ClientOnly>
 </template>
 
 <script setup>
